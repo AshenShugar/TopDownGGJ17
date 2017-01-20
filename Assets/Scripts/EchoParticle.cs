58c608d8ae992ef32a7117a0e7cd199768a69665
Peter J Armstrong <ashen_s@yahoo.com.au>
2017-01-20 23:45:32 +1100

    Setup Menu to make it easy to go to the test scenes while developing. Beginning of scripts to do echo-location

---
+++ Assets/Scripts/EchoParticle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoParticle : MonoBehaviour {
+
+
+	private Vector3 _MovementDirection;
+
+
+	public Vector3 MovementDirection {
+		get {
+			return _MovementDirection;
+		}
+		set {
+			_MovementDirection = value;
+		}
+	}
+
+	public EchoParticle( Vector3 initialDirection)
+	{
+		MovementDirection = initialDirection;
+	}
+
+	void OnCollisionEnter(Collision aCollision)
+	{
+		VisibilityScript vScript = aCollision.gameObject.GetComponent<VisibilityScript> ();
+
+		vScript.ChangeAlpha (0.5f);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + (MovementDirection * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/LaunchEcho.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public int NumberOfEchoParticles;
+
+	public int angleOffFoward = 30;
+
+
+	public void LaunchEchoLocation()
+	{
+		Quaternion angleOfLaunch;
+
+		Vector3 LaunchVector;
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
