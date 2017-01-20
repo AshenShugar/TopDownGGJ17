4d08ae95ac2a002fc7ee447f061700c20b5aee07
Peter J Armstrong <ashen_s@yahoo.com.au>
2017-01-20 22:18:28 +1100

    Super basic splash scene, empty menu scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+	public int NumberOfPlayers = 2;
+	public int CurrentPlayer = 0;
+	public int MaxNumberOfPlayers = 4;
+
+
+	public string CurrentPlayerText {
+		get {
+			return (CurrentPlayer + 1).ToString ();
+		}
+	}
+
+	public void NextPlayer()
+	{
+		CurrentPlayer++;
+		if (CurrentPlayer >= NumberOfPlayers)
+			CurrentPlayer = 0;
+	}
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
