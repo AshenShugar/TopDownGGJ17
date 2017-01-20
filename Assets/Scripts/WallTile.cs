8aef7676ca7999a8d8243304bd435a859ffd3eaa
Anthony Liew <glider521al@gmail.com>
2017-01-20 23:18:48 +1100

    world manager, basic procedural generation with population script, new scene

---
+++ Assets/Scripts/WallTile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTile : MonoBehaviour {
+
+    public int TileX
+    {
+        get
+        {
+            return ((int)Mathf.Round(transform.position.x));
+        }
+    }
+
+    public int TileY
+    {
+        get
+        {
+            return ((int)Mathf.Round(transform.position.y));
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (WorldController.instance != null)
+        {
+            //WorldController.instance.tileIds[TileX, TileY] = 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    void OnDestroy()
+    {
+        
+        if (WorldController.instance != null)
+        {
+            //WorldController.instance.tileIds[TileX, TileY] = 0;
+        }
+    }
+}
