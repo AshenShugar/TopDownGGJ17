bcf65af1b7c1e0548578b8a46e782583f80e8084
Anthony Liew <glider521al@gmail.com>
2017-01-21 14:33:47 +1100

    Simple script follow player

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour {
+    GameObject following;
+	// Use this for initialization
+	void Awake () {
+        following = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = following.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, -10);
+	}
+}
