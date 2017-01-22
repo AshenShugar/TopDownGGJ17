7281d602c28daa6b36d30e3f7e8039424441786d
Anthony Liew <glider521al@gmail.com>
2017-01-22 15:24:18 +1100

    Spawn enemies every 20 seconds at the top of the level

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LevelController : MonoBehaviour {
     public string NextLevelName;
     private float DelayTillNextLevel = 3.0f;
+    public List<GameObject> spawnedEnemies = new List<GameObject>();
+    public int maxEnemies = 5;
+    public int maxDelayBeforeSpawnEnemy=UGameLogic.lengthOfSecond*10;
+    public int delayBeforeEnemySpawn = 0;
+    public GameObject instanciateEnemy;
+    public List<Vector3> spawnEnemyPositions = new List<Vector3>();
+    public System.Random spawnRandom = new System.Random();
     public static LevelController instance;
     // Use this for initialization
     void Start () {
@@ -24,7 +32,28 @@ public class LevelController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        delayBeforeEnemySpawn--;
+        if(delayBeforeEnemySpawn<1)
+        {
+            delayBeforeEnemySpawn = maxDelayBeforeSpawnEnemy;
+            for (int i = 0; i < spawnedEnemies.Count; i++)
+            {
+                if (spawnedEnemies[i] == null)
+                {
+                    spawnedEnemies.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (instanciateEnemy != null && spawnEnemyPositions.Count > 0 && spawnedEnemies.Count < maxEnemies)
+            {
+                GameObject enemy = GameObject.Instantiate(instanciateEnemy);
+                Enemy1 es = enemy.GetComponent<Enemy1>();
+                es.wakeUp();
+                spawnedEnemies.Add(enemy);
+                enemy.transform.position = spawnEnemyPositions[spawnRandom.Next(0, spawnEnemyPositions.Count)];
+            }
+        }
 	}
 
     public void CheckLevelEnd()
@@ -32,6 +61,7 @@ public class LevelController : MonoBehaviour {
         GoalBug[] bugs = GameObject.FindObjectsOfType<GoalBug>();
         if (bugs.Length == 0)
         {
+
 
 
             Invoke("LoadNext", DelayTillNextLevel);
